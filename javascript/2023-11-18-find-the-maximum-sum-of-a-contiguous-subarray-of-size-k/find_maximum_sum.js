586a9910bbf15d4b3cadab3447def6e7e01381a7
David Costa <davidcostadev@gmail.com>
2023-11-18 15:19:42 -0300

    Saving the solution

--- javascript/2023-11-18-find-the-maximum-sum-of-a-contiguous-subarray-of-size-k/find_maximum_sum.js
+++ javascript/2023-11-18-find-the-maximum-sum-of-a-contiguous-subarray-of-size-k/find_maximum_sum.js
@@ -14,11 +14,11 @@ function test(testName, callback) {
 }
 
 test("Find Maximum Sum Test", function() {
-assert(findMaximumSum([2, 3, 4, 1, 5], 2), 7, "Test 1");
-assert(findMaximumSum([2, 1, 5, 1, 3, 2], 3), 9, "Test 2");
-assert(findMaximumSum([4], 1), 4, "Test 3");
-assert(findMaximumSum([1, 2, 3, 4, 5], 5), 15, "Test 4");
-assert(findMaximumSum([-1, -2, 3, 4, -5], 2), 7, "Test 5");
+  assert(findMaximumSum([2, 3, 4, 1, 5], 2), 7, "Test 1");
+  assert(findMaximumSum([2, 1, 5, 1, 3, 2], 3), 9, "Test 2");
+  assert(findMaximumSum([4], 1), 4, "Test 3");
+  assert(findMaximumSum([1, 2, 3, 4, 5], 5), 15, "Test 4");
+  assert(findMaximumSum([-1, -2, 3, 4, -5], 2), 7, "Test 5");
 });
 
 
