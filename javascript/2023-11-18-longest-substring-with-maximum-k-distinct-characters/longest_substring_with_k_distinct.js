f2c2ea4b62bafced41c027019ca41c51be484e95
David Costa <davidcostadev@gmail.com>
2023-11-19 14:57:27 -0300

    Improving tests

--- javascript/2023-11-18-longest-substring-with-maximum-k-distinct-characters/longest_substring_with_k_distinct.js
+++ javascript/2023-11-18-longest-substring-with-maximum-k-distinct-characters/longest_substring_with_k_distinct.js
@@ -1,17 +1,16 @@
-function assert(condition, message) {
-  if (!condition) {
-      throw new Error(message || 'Assertion failed');
-  }
-}
+const { assert, test } = require("../lib/testUtils");
 
-// Test cases
-assert(longest_substring_with_k_distinct("araaci", 2) === 4, "Test 1 Failed");
-assert(longest_substring_with_k_distinct("araaci", 1) === 2, "Test 2 Failed");
-assert(longest_substring_with_k_distinct("cbbebi", 3) === 5, "Test 3 Failed");
-assert(longest_substring_with_k_distinct("aabacbebebe", 3) === 7, "Test 4 Failed");
-assert(longest_substring_with_k_distinct("aaaa", 2) === 4, "Test 5 Failed");
-
-console.log("All tests passed!");
+test("Longest substring with k distinct", () => {
+  assert(longest_substring_with_k_distinct("araaci", 2), 4, "Test 1 Failed");
+  assert(longest_substring_with_k_distinct("araaci", 1), 2, "Test 2 Failed");
+  assert(longest_substring_with_k_distinct("cbbebi", 3), 5, "Test 3 Failed");
+  assert(
+    longest_substring_with_k_distinct("aabacbebebe", 3),
+    7,
+    "Test 4 Failed",
+  );
+  assert(longest_substring_with_k_distinct("aaaa", 2), 4, "Test 5 Failed");
+});
 
 function longest_substring_with_k_distinct(s, k) {
   // Your code here
