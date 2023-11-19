a1218cfe6a9981843e8dea9ca19a5c7da66ed8b8
David Costa <davidcostadev@gmail.com>
2023-11-19 14:53:31 -0300

    Improving assert

--- javascript/lib/testUtils.js
+++ javascript/lib/testUtils.js
@@ -7,12 +7,18 @@ function assert(actual, expected, message) {
 function test(testName, callback) {
   try {
     callback();
-    console.log(`\x1b[32m%s\x1b[0m`, `\n${testName}:\n\t Passed`);
+    console.log(
+      `\x1b[42m\x1b[30m%s\x1b[0m - \x1b[32m%s\x1b[0m`,
+      `PASSED`,
+      testName,
+    );
   } catch (e) {
     console.error(
-      `\x1b[31m%s\x1b[0m`,
-      `\n${testName}:\n\t Failed - ${e.message}`,
+      `\x1b[41m\x1b[30m%s\x1b[0m - \x1b[31m%s\x1b[0m`,
+      `FAILED`,
+      testName,
     );
+    console.error(`    - \x1b[31m%s\x1b[0m`, e.message);
   }
 }
 
