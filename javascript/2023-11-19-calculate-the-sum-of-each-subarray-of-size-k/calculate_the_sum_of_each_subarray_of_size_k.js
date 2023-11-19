ff7ee41f8a93a31b00cb2538fc6526c4319dae91
David Costa <davidcostadev@gmail.com>
2023-11-19 15:11:56 -0300

    Removing solution from the code

--- javascript/2023-11-19-calculate-the-sum-of-each-subarray-of-size-k/calculate_the_sum_of_each_subarray_of_size_k.js
+++ javascript/2023-11-19-calculate-the-sum-of-each-subarray-of-size-k/calculate_the_sum_of_each_subarray_of_size_k.js
@@ -7,13 +7,5 @@ test("Sum of Each Subarray of Size K", function () {
 });
 
 function findSumOfSubarray(arr, K) {
-  const sumArray = [];
-  for (let i = 0; i <= arr.length - K; i++) {
-    let sumLoop = 0;
-    for (let j = i; j < i + K; j++) {
-      sumLoop += arr[j];
-    }
-    sumArray.push(sumLoop);
-  }
-  return sumArray;
+  // Your code here
 }
